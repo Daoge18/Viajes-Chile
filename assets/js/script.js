044fed247d37ca307a6ea78902b418edaedeeb66
die_b <daoge31@gmail.com>
2024-02-29 18:52:46 -0300

    Copia de archivos a GitHub

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$("#enviar").click(function(){
+    alert("El mensaje fue enviado correctamente...");
+});
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+    if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+    scrollTop: $(hash).offset().top
+    }, 800, function(){
+   
+    window.location.hash = hash;
+    });
+    }
+    });
+   });
